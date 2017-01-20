459f7fa795b6f85a3872de4b27fb2e5b673bf45d
U-D-36-07\gamejam <D-36-07+gamejam@D-36-07.bellecour.local>
2017-01-21 00:05:12 +0100

    Working and moving on pooling and enemy units.

--- Assets/Scripts/PoolSystem/PoolManager.cs
+++ Assets/Scripts/PoolSystem/PoolManager.cs
@@ -12,8 +12,12 @@ public class PoolData
     [SerializeField]
     private GameObject prefab = null;
 
+    [SerializeField]
+    private string name = "";
+
     public int Quantity { get { return quantity; } }
     public GameObject Prefab { get { return prefab; } }
+    public string Name { get { return name; } }
 
     public bool IsValid { get { return quantity != -1 && prefab != null; } }
 }
@@ -21,10 +25,26 @@ public class PoolData
 public class PoolManager : MonoBehaviour {
     [SerializeField]
     public List<PoolData> poolsToSpawn;
-    
+
+    private List<Pool> pools;
+
     private void Awake()
     {
+        pools = new List<Pool>();
+
         poolsToSpawn.Where(pool => pool.IsValid)
-            .ForEach(data => Debug.LogFormat("Prefab: {0}, Quantity: {1}", data.Prefab.name, data.Quantity));
-    }    
+            .ForEach(data =>
+            {
+                pools.Add(CreatePool(data));
+            });
+    }
+    
+    private Pool CreatePool(PoolData data)
+    {
+        Pool p = new GameObject().AddComponent<Pool>();
+        p.Init(data.Quantity, data.Prefab.GetComponent<Poolable>());
+        p.name = string.Format("Pool_{0}_{1}", data.Prefab.name, data.Name);
+        p.transform.SetParent(transform);
+        return p;
+    }
 }
