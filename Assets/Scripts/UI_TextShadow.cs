1e6d110e0350c706da2eab4054599c0b02043ce3
gamejam <gamejam>
2017-01-20 23:03:53 +0100

    Vaisseau, camera scripts, debut de UI

---
+++ Assets/Scripts/UI_TextShadow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public partial class UI_TextShadow : MonoBehaviour {
+
+	private Text selfText;
+	private Text parentText;
+
+	private RectTransform selfRT;
+	private RectTransform parentRT;
+
+	void Start () {
+
+		GetTexts ();
+	}
+
+	void Update () {
+
+		selfText.text = parentText.text;
+	}
+
+	void GetTexts () {
+
+		if (!selfText)
+			selfText = GetComponent<Text> ();
+		if (!parentText)
+			parentText = transform.parent.GetComponent<Text> ();
+	}
+
+//	void GetRTs () {
+//
+//		if (!selfRT)
+//			selfRT = GetComponent<RectTransform> ();
+//		if (!parentRT)
+//			parentRT = GetComponent<RectTransform> ();
+//	}
+}
---
+++ Assets/Scripts/UI_TextShadow_Editor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
+public partial class UI_TextShadow : MonoBehaviour {
+
+	void OnDrawGizmos () {
+
+		if (!selfText || !parentText)
+			GetTexts ();
+
+//		if (!selfRT || !parentRT)
+//			GetRTs ();
+
+		selfText.text = parentText.text;
+	}
+}
