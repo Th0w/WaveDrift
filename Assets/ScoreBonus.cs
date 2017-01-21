dfcc9cfdb2b2151e62659b3245136381c0becb5b
gamejam <gamejam@D-36-07>
2017-01-21 12:40:57 +0100

    Clean stuff and add lootz

--- Assets/ScoreBonus.cs
+++ Assets/ScoreBonus.cs
@@ -1,22 +1,26 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
-using UnityEngine;
+using System.Text.RegularExpressions;
 
 [RequireComponent(typeof(CapsuleCollider))]
-public class MultiplierBonus : MonoBehaviour {
+public class ScoreBonus : MonoBehaviour
+{
     [SerializeField]
-    private int multiplierValue;
-	// Use this for initialization
-	void Start () {
+    private int scoreValue;
+    // Use this for initialization
+    void Start()
+    {
         Regex regex = new Regex(@"^Ship_P(\w)$");
         this.OnTriggerEnterAsObservable()
             .Select(collider => regex.Match(collider.name))
             .Where(match => match.Success)
             .Subscribe(match => {
                 int pid = int.Parse(match.Groups[0].Value);
-                MessagingCenter.Instance.FireMessage("AddPlayerScoreMultiplier", new object[] { pid, multiplierValue });
+                MessagingCenter.Instance.FireMessage("AddPlayerScoreMultiplier", new object[] { pid, scoreValue });
                 Destroy(gameObject);
             }).AddTo(this);
-	}
+    }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,6 +16,7 @@ public class ScoreManager : MonoBehaviour {
         MessagingCenter.Instance.RegisterMessage("UnitKilled", HandleUnitKilled);
         MessagingCenter.Instance.RegisterMessage("UnitTookDamage", HandleUnitTookDamage);
         MessagingCenter.Instance.RegisterMessage("AddPlayerScoreMultiplier", HandlePlayerScoreMultiplier);
+        MessagingCenter.Instance.RegisterMessage("PlayerGainScore", GainScore);
     }
 
     private void OnDestroy()
@@ -44,13 +42,7 @@ public class ScoreManager : MonoBehaviour {
             Debug.LogError("Wrong arguments passed.");
             return;
         }
-        var nobj = (object[])obj;
-        int pid = (int)nobj[0] - 1;
-        int score = (int)nobj[1];
-        playerCachedScore[pid] += score * playerScoreBonusMultiplier[pid];
-        string str = ("000000" + playerCachedScore[pid].ToString());
-        str = str.Substring(str.Length - 6);
-        playerScores[pid].text = str;
+        GainScore(ObjectToValues(obj));
     }
 
     private void HandleUnitKilled(object obj)
@@ -58,14 +50,34 @@ public class ScoreManager : MonoBehaviour {
         if (obj is object[] == false)
         {
             Debug.LogWarning("Wrong arguments passed.");
+            return;
         }
-        var nobj = (object[])obj;
-        int pid = (int)nobj[0] - 1;
-        int score = (int)nobj[1];
-        playerCachedScore[pid] += score * playerScoreBonusMultiplier[pid];
-        string str = ("000000" + playerCachedScore[pid].ToString());
-        str = str.Substring(str.Length - 6);
-        playerScores[pid].text = str;
+        GainScore(ObjectToValues(obj));
+    }
 
+    private void GainScore(object obj)
+    {
+        if (obj is object[] == false)
+        {
+            Debug.LogWarning("Wrong arguments passed.");
+            return;
+        }
+        GainScore(ObjectToValues(obj));
+    }
+
+    private void GainScore(Tuple<int, int> values)
+    {
+        playerCachedScore[values.Item1] += values.Item2 * playerScoreBonusMultiplier[values.Item1];
+        string str = ("000000" + playerCachedScore[values.Item1].ToString());
+        str = str.Substring(str.Length - 6);
+        playerScores[values.Item1].text = str;
+    }
+
+    private Tuple<int, int> ObjectToValues(object obj)
+    {
+        var nobj = (object[])obj;
+        var id = (int)nobj[0] - 1;
+        var score = (int)nobj[1];
+        return new Tuple<int, int>(id, score);
     }
 }
