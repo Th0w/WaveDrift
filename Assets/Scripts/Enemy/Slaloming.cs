71cdb82356cd1ca2225999dfc0dd3efa9d75de0d
gamejam <gamejam@D-36-07>
2017-01-21 03:33:25 +0100

    Fixed slaloming enemy

--- Assets/Scripts/Enemy/Slaloming.cs
+++ Assets/Scripts/Enemy/Slaloming.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Slaloming : SimpleMover {
     private float progress;
+    [SerializeField]
+    private Transform childObject;
+
+    private BoxCollider bc;
+
+    [SerializeField]
+    private float slalomAmplitude;
 
     protected override void MoveFunction(Vector3 distance)
     {
         progress += Time.deltaTime;
 
+        bc = bc ?? GetComponent<BoxCollider>();
+
         transform.rotation = Quaternion.LookRotation(distance);
         Debug.LogFormat("Current cos: {0}", Mathf.Cos(progress));
-        transform.position += transform.forward * Time.deltaTime * speed + transform.right * Mathf.Cos(progress) / Mathf.PI;
+        transform.position += transform.forward * Time.deltaTime * speed;
+        Vector3 delta = Vector3.right * Mathf.Cos(progress) * slalomAmplitude;
+        childObject.transform.localPosition = delta;
+        delta.z = bc.center.z;
+        bc.center = delta;
     }
 }
