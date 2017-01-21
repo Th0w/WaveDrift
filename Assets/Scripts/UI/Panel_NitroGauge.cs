8bf85480e7c398d24c2711e197259f042983033f
gamejam <gamejam>
2017-01-21 06:26:01 +0100

    Over glow et debut de DA

---
+++ Assets/Scripts/UI/Panel_NitroGauge.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel_NitroGauge : MonoBehaviour {
+
+	public Transform player;
+	public Camera mainCamera;
+
+	public RectTransform mainCanvas;
+	public Image fillImage;
+
+	public Vector2 offset;
+
+	private RectTransform selfRT;
+
+	void Start () {
+
+		selfRT = GetComponent<RectTransform> ();
+
+		if (!player.gameObject.activeSelf)
+			gameObject.SetActive (false);
+	}
+
+	void Update () {
+
+		Vector2 pos = mainCamera.WorldToViewportPoint (player.position);
+		pos.x *= Screen.width / mainCanvas.localScale.x;
+		pos.y *= Screen.height / mainCanvas.localScale.y;
+		selfRT.anchoredPosition = pos + offset;
+	}
+}
--- Assets/Shaders/SHD_OpaqueFade.shader
+++ Assets/Shaders/SHD_OpaqueFade.shader
@@ -6,6 +6,8 @@
 		_FresnelColor ("Fresnel color", Color) = (1,1,1,1)
 
 		_FresnelPow ("Fresnel power", Float) = 1
+
+		_OverGlow ("OverGlow", Float) = 0
 	}
 
 	SubShader {
@@ -18,6 +20,8 @@
 
 		float _FresnelPow;
 
+		float _OverGlow;
+
 		struct Input {
 
 			float3 viewDir;
@@ -28,7 +32,7 @@
 			o.Albedo = 0;
 
 			float fresnel = pow(dot(IN.viewDir, o.Normal), _FresnelPow);
-			o.Emission = _MainColor.rgb + _FresnelColor.rgb * fresnel;
+			o.Emission = _MainColor.rgb + _FresnelColor.rgb * fresnel * _OverGlow;
 		}
 		ENDCG
 	}
