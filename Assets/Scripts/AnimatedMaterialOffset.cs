a4bf6727fe8dd1b03261b3331b6adeafe0bad435
gamejam <gamejam>
2017-01-22 13:09:54 +0100

    DA power up + fix hub tuto

---
+++ Assets/Scripts/AnimatedMaterialOffset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AnimatedMaterialOffset : MonoBehaviour {
+
+	private Renderer rend;
+	public Vector2 offset;
+
+	void Start () {
+
+		rend = GetComponent<Renderer> ();
+	}
+
+	void Update () {
+
+		rend.material.SetTextureOffset ("_MainTex", offset * Time.timeSinceLevelLoad);
+	}
+}
