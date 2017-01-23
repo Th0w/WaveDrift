07039bbcb2164b029665a7435355c3606835085e
Vincent Bastioni <v.bastioni@gmail.com>
2017-01-23 19:57:32 +0100

    Add keyboards controls

--- Assets/Scripts/ShipBehaviour_V2.cs
+++ Assets/Scripts/ShipBehaviour_V2.cs
@@ -9,355 +9,283 @@ using UnityEngine.UI;
 public class ShipBehaviour_V2 : MonoBehaviour
 {
 
-	// PLAYER
-	public enum Players { Player1, Player2, Player3, Player4 }
-	[Header("PLAYER")]
-	[Space(10)]
-	public Players player;
-	public int playerID;
-	public Vector3 spawnPos;
+    // PLAYER
+    public enum Players { Player1, Player2, Player3, Player4 }
+    [Header("PLAYER")]
+    [Space(10)]
+    public Players player;
+    public int playerID;
+    public Vector3 spawnPos;
 
-	// INPUTS
-	[Header("INPUTS")]
-	[Space(10)]
-	[Range(0f, 1f)]
-	public float leftStickHorizontalInput;
-	[Range(0f, 1f)]
-	public float rightTriggerInput;
+    // INPUTS
+    [Header("INPUTS")]
+    [Space(10)]
+    [Range(0f, 1f)]
+    public float leftStickHorizontalInput;
+    [Range(0f, 1f)]
+    public float rightTriggerInput;
 
-	// SHIP
-	[Header("SHIP")]
-	[Space(10)]
-	public Transform ship;
-	[Space(6)]
-	public float speed;
-	public float speedLerprate;
-	public float actualSpeed;
-	[Space(6)]
-	public float rotStrength;
-	public float additionnalDriftRotStrength;
-	public float rotLerpRate;
-	public float actualRotStrength;
-	[Space(6)]
-	public float minRotToDrift;
-	public bool drift;
-	public bool cooldown;
-	[Space(6)]
-	public bool jump;
-	public bool airProtection;
+    // SHIP
+    [Header("SHIP")]
+    [Space(10)]
+    public Transform ship;
+    [Space(6)]
+    public float speed;
+    public float speedLerprate;
+    public float actualSpeed;
+    [Space(6)]
+    public float rotStrength;
+    public float additionnalDriftRotStrength;
+    public float rotLerpRate;
+    public float actualRotStrength;
+    [Space(6)]
+    public float minRotToDrift;
+    public bool drift;
+    public bool cooldown;
+    [Space(6)]
+    public bool jump;
+    public bool airProtection;
 
-	//DRIFT
-	[Header("DRIFT")]
-	[Space(10)]
-	public float driftTime;
-	public float maxDriftTime;
-	public float driftRecoveryFactor = 1;
-	public float driftLossRatio = 1;
-	[Space(6)]
-	public Image driftGauge;
+    //DRIFT
+    [Header("DRIFT")]
+    [Space(10)]
+    public float driftTime;
+    public float maxDriftTime;
+    public float driftRecoveryFactor = 1;
+    public float driftLossRatio = 1;
+    [Space(6)]
+    public Image driftGauge;
 
-	// PARTICLE SYSTEMS
-	[Header("PARTICLE SYSTEMS")]
-	[Space(10)]
-	public ParticleSystem[] firePS;
-	public ParticleSystem[] driftPS;
-	[Space(6)]
-	public GameObject deathGroup;
+    // PARTICLE SYSTEMS
+    [Header("PARTICLE SYSTEMS")]
+    [Space(10)]
+    public ParticleSystem[] firePS;
+    public ParticleSystem[] driftPS;
+    [Space(6)]
+    public GameObject deathGroup;
 
-	// MISC
-	[Header("PARTICLE SYSTEMS")]
-	[Space(10)]
-	public bool death;
-	public float deathDelay;
-	public bool invulnerability;
-	public float spawnTimeProtection;
-	[Space(6)]
-	public Transform barrier;
-	private Renderer barrierRenderer;
+    // MISC
+    [Header("PARTICLE SYSTEMS")]
+    [Space(10)]
+    public bool death;
+    public float deathDelay;
+    public bool invulnerability;
+    public float spawnTimeProtection;
+    [Space(6)]
+    public Transform barrier;
+    private Renderer barrierRenderer;
 
-	[Header("PowerUps")]
-	[Space(10)]
-	public GameObject driftMegaMegaGauge;
-	public Color powerUpWaveEmitColor;
-	public bool shield;
-	[HideInInspector]
-	public Material baseMat;
-	public Material shieldMat;
+    [Header("PowerUps")]
+    [Space(10)]
+    public GameObject driftMegaMegaGauge;
+    public Color powerUpWaveEmitColor;
+    public bool shield;
+    [HideInInspector]
+    public Material baseMat;
+    public Material shieldMat;
 
-	// PRIVATE
-	private Rigidbody selfRB;
-	public Vector2 textureOffsetFactor;
-	private Animator selfAnimator;
-	[HideInInspector]
-	private UI_DeathOL deathOL;
-	[HideInInspector]
-	public Coroutine cor;
+    // PRIVATE
+    private Rigidbody selfRB;
+    public Vector2 textureOffsetFactor;
+    private Animator selfAnimator;
+    [HideInInspector]
+    private UI_DeathOL deathOL;
+    [HideInInspector]
+    public Coroutine cor;
 
-	private IDisposable deathDisposable, invulDisposable;
+    private IDisposable deathDisposable, invulDisposable;
 
-	private Player thePlayer;
+    private Player thePlayer;
 
-	public bool IsFrozen { get; set; }
+    public bool IsFrozen { get; set; }
 
-	void Start()
-	{
+    void Start() {
 
-		playerID = (int)player;
-		//if (player == Players.Player1)
-		//	playerID = 0;
-		//else if (player == Players.Player2)
-		//	playerID = 1;
-		//else if (player == Players.Player3)
-		//	playerID = 2;
-		//else
-		//	playerID = 3;
+        playerID = (int)player;
 
-		thePlayer = ReInput.players.GetPlayer(playerID);
+        thePlayer = ReInput.players.GetPlayer(playerID);
 
-		selfRB = GetComponent<Rigidbody>();
-		selfAnimator = GetComponent<Animator>();
+        selfRB = GetComponent<Rigidbody>();
+        selfAnimator = GetComponent<Animator>();
 
-		driftTime = maxDriftTime;
+        driftTime = maxDriftTime;
 
-		if (barrier)
-			barrierRenderer = barrier.GetComponent<Renderer>();
+        if (barrier)
+            barrierRenderer = barrier.GetComponent<Renderer>();
 
-		baseMat = ship.GetComponent<MeshRenderer> ().material;
+        baseMat = ship.GetComponent<MeshRenderer>().material;
 
-		deathOL = FindObjectOfType<UI_DeathOL>();
-		IsFrozen = false;
-
-		this.OnParticleTriggerAsObservable()
-			//.Where(_ => IsFrozen)
-			.Subscribe(_ => Debug.Log("SHOULD UNFROZE!"))
-			.AddTo(this);
+        deathOL = FindObjectOfType<UI_DeathOL>();
+        IsFrozen = false;
     }
 
-	void Update()
-	{
-		if (thePlayer.GetButtonTimedPressDown("Start", 2.0f))
-		{
-			GameManager.Instance.Reset();
-		}
+    void Update() {
+        if (thePlayer.GetButtonTimedPressDown("Start", 2.0f)) {
+            GameManager.Instance.Reset();
+        }
 
-		if (thePlayer.GetButtonTimedPressDown("Select", 2.0f))
-		{
-			Application.Quit ();
-		}
+        if (thePlayer.GetButtonTimedPressDown("Select", 2.0f)) {
+#if UNITY_EDITOR
+            Debug.Log("QUITTING PLAYMODE");
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
 
-		if (IsFrozen == true)
-		{
-			if (thePlayer.GetButtonDown("Button A"))
-			{
-				GameManager.Instance.Unfreeze(playerID);
-				Debug.Log("UNFREEZING!");
-			}
-			return;
-		}
-		// DEV CHEATS
-		if (Input.GetKeyDown(KeyCode.T))
-			Death();
+        if (IsFrozen == true) {
+            if (thePlayer.GetButtonDown("Button A")) {
+                GameManager.Instance.Unfreeze(playerID);
+                Debug.Log("UNFREEZING!");
+            }
+            return;
+        }
+        // DEV CHEATS
+        if (Input.GetKeyDown(KeyCode.T))
+            Death();
 
-		// Out of bounds!
-		if (transform.position.magnitude > 177 && !death) {
-			shield = false;
-			Death ();
-		}
-		// DEATH LOCK!!
-		if (death)
-			return;
+        // Out of bounds!
+        if (transform.position.magnitude > 177 && !death) {
+            shield = false;
+            Death();
+        }
+        // DEATH LOCK!!
+        if (death)
+            return;
 
-		// Inputs
-		//rightTriggerInput = Input.GetAxis (playerPrefix + "RightTrigger");
-		//leftStickHorizontalInput = Input.GetAxis (playerPrefix + "LeftStick_Horizontal");
-		rightTriggerInput = thePlayer.GetAxis("Right Trigger");
-		leftStickHorizontalInput = thePlayer.GetAxis("Left Stick X");
+        // Inputs
+        //rightTriggerInput = Input.GetAxis (playerPrefix + "RightTrigger");
+        //leftStickHorizontalInput = Input.GetAxis (playerPrefix + "LeftStick_Horizontal");
+        rightTriggerInput = thePlayer.GetAxis("Right Trigger");
+        leftStickHorizontalInput = thePlayer.GetAxis("Left Stick X");
 
-		// Drift input
-		float targetRotStrength = rotStrength;
-		//if (Input.GetButton (playerPrefix + "Button_X")) 
-		if (thePlayer.GetButton("Button X"))
-		{
+        // Drift input
+        float targetRotStrength = rotStrength;
+        //if (Input.GetButton (playerPrefix + "Button_X"))
+        if (thePlayer.GetButton("Button X")) {
 
-			targetRotStrength += additionnalDriftRotStrength;
-			drift = true;
-		}
-		else
-			drift = false;
+            targetRotStrength += additionnalDriftRotStrength;
+            drift = true;
+        } else {
+            drift = false;
+        }
 
-		// Speed & rot lerps
-		actualSpeed = Mathf.Lerp(actualSpeed, speed * rightTriggerInput, speedLerprate * Time.deltaTime);
-		actualRotStrength = Mathf.Lerp(actualRotStrength, leftStickHorizontalInput * targetRotStrength, rotLerpRate * Time.deltaTime);
+        // Speed & rot lerps
+        actualSpeed = Mathf.Lerp(actualSpeed, speed * rightTriggerInput, speedLerprate * Time.deltaTime);
+        actualRotStrength = Mathf.Lerp(actualRotStrength, leftStickHorizontalInput * targetRotStrength, rotLerpRate * Time.deltaTime);
 
-		// Motion
-		if (!jump)
-		{
-			selfRB.velocity += transform.forward * actualSpeed;
-			selfRB.velocity *= 0.9f;
-			transform.localEulerAngles += new Vector3(0, actualRotStrength, 0);
-		}
-		else
-			transform.localEulerAngles += new Vector3(0, actualRotStrength * 2, 0);
+        // Motion
+        if (!jump) {
+            selfRB.velocity += transform.forward * actualSpeed;
+            selfRB.velocity *= 0.9f;
+            transform.localEulerAngles += new Vector3(0, actualRotStrength, 0);
+        } else {
+            transform.localEulerAngles += new Vector3(0, actualRotStrength * 2, 0);
+        }
 
-		// Jump
-		//if (Input.GetButtonDown (playerPrefix + "Button_A") && !jump) 
-		if (thePlayer.GetButtonDown("Button A") && !jump)
-		{
+        // Jump
+        //if (Input.GetButtonDown (playerPrefix + "Button_A") && !jump) 
+        if (thePlayer.GetButtonDown("Button A") && !jump) {
 
-			selfAnimator.Play("Anim_Ship_Jump", 0, 0);
-			jump = true;
-			airProtection = true;
-		}
+            selfAnimator.Play("Anim_Ship_Jump", 0, 0);
+            jump = true;
+            airProtection = true;
+        }
 
-		// Fire PS
-		foreach (ParticleSystem ps in firePS)
-			ps.emissionRate = actualSpeed / speed * 128f;
+        // Fire PS
+        foreach (ParticleSystem ps in firePS) {
+            ps.emissionRate = actualSpeed / speed * 128f;
+        }
 
-		// Drifts!
-		if (drift && Mathf.Abs(actualRotStrength) > minRotToDrift && driftTime > 0 && !cooldown && !jump)
-		{
-			driftTime = Mathf.Clamp(driftTime - Time.deltaTime * driftLossRatio, 0, maxDriftTime);
+        // Drifts!
+        if (drift && Mathf.Abs(actualRotStrength) > minRotToDrift && driftTime > 0 && !cooldown && !jump) {
+            driftTime = Mathf.Clamp(driftTime - Time.deltaTime * driftLossRatio, 0, maxDriftTime);
 
-			if (driftTime == 0)
-				cooldown = true;
+            if (driftTime == 0) {
+                cooldown = true;
+            }
 
-			if (actualRotStrength > 0)
-			{ // Rot left
+            if (actualRotStrength > 0) { // Rot left
 
-				driftPS[0].emissionRate = 256;
-				driftPS[1].emissionRate = 0;
+                driftPS[0].emissionRate = 256;
+                driftPS[1].emissionRate = 0;
 
-			}
-			else
-			{ // Rot right
+            } else { // Rot right
 
-				driftPS[0].emissionRate = 0;
-				driftPS[1].emissionRate = 256;
-			}
-		}
-		else
-		{
+                driftPS[0].emissionRate = 0;
+                driftPS[1].emissionRate = 256;
+            }
+        } else {
 
-			driftTime = Mathf.Clamp(driftTime + Time.deltaTime * driftRecoveryFactor, 0, maxDriftTime);
+            driftTime = Mathf.Clamp(driftTime + Time.deltaTime * driftRecoveryFactor, 0, maxDriftTime);
 
-			if (cooldown && driftTime == maxDriftTime)
-				cooldown = false;
+            if (cooldown && driftTime == maxDriftTime) {
+                cooldown = false;
+            }
 
-			foreach (ParticleSystem ps in driftPS)
-				ps.emissionRate = 0;
-		}
+            foreach (ParticleSystem ps in driftPS) {
+                ps.emissionRate = 0;
+            }
+        }
 
-		// Drift gauge
-		driftGauge.fillAmount = driftTime / maxDriftTime;
-		if (cooldown)
-			driftGauge.color = new Color((Mathf.Sin(Time.timeSinceLevelLoad * 12) + 1) / 2, 0, 0, 1);
-		else
-			driftGauge.color = Color.white;
+        // Drift gauge
+        driftGauge.fillAmount = driftTime / maxDriftTime;
 
-		// Barrier
-		barrier.transform.position = new Vector3(0, transform.position.y, 0);
-		barrier.transform.rotation = Quaternion.FromToRotation(Vector3.forward, transform.position);
-		barrierRenderer.material.SetTextureOffset("_MainTex", new Vector2(barrier.transform.localEulerAngles.y * textureOffsetFactor.x, transform.position.y * textureOffsetFactor.y));
-		barrierRenderer.material.SetFloat("_GlobalAlpha", Mathf.InverseLerp(145f, 175f, transform.position.magnitude));
-	}
+        if (cooldown) {
+            driftGauge.color = new Color((Mathf.Sin(Time.timeSinceLevelLoad * 12) + 1) / 2, 0, 0, 1);
+        } else {
+            driftGauge.color = Color.white;
+        }
 
-	/*public void KillPlayer() {
-		
-		SlowMo.selfAnimator.Play ("Anim_SlowMo", 0, 0);
+        // Barrier
+        barrier.transform.position = new Vector3(0, transform.position.y, 0);
+        barrier.transform.rotation = Quaternion.FromToRotation(Vector3.forward, transform.position);
+        barrierRenderer.material.SetTextureOffset("_MainTex", new Vector2(barrier.transform.localEulerAngles.y * textureOffsetFactor.x, transform.position.y * textureOffsetFactor.y));
+        barrierRenderer.material.SetFloat("_GlobalAlpha", Mathf.InverseLerp(145f, 175f, transform.position.magnitude));
+    }
 
-		ship.gameObject.SetActive (false);
-		deathGroup.SetActive (true);
+    public void Death() {
+        if (!shield) {
+            if (cor != null) {
+                StopCoroutine(cor);
+            }
+            cor = StartCoroutine(CoDeath());
+        }
+    }
 
-		foreach (ParticleSystem ps in firePS)
-			ps.emissionRate = 0;
-		foreach (ParticleSystem ps in driftPS)
-			ps.emissionRate = 0;
+    public IEnumerator CoDeath() {
+        string id = player.ToString();
+        id = id.Substring(id.Length - 1);
+        Vector3 deathPos = transform.position;
 
-		selfRB.velocity = Vector3.zero;
+        SlowMo.selfAnimator.Play("Anim_SlowMo", 0, 0);
 
-		driftGauge.fillAmount = 0;
+        ship.gameObject.SetActive(false);
+        ship.GetComponent<MeshRenderer>().material = baseMat;
+        deathGroup.SetActive(true);
 
-		actualSpeed = 0;
-		actualRotStrength = 0;
+        foreach (ParticleSystem ps in firePS) {
+            ps.emissionRate = 0;
+        }
+        foreach (ParticleSystem ps in driftPS) {
+            ps.emissionRate = 0;
+        }
 
-		death = true;
-		jump = false;
-		invulnerability = true;
+        selfRB.velocity = Vector3.zero;
 
-		deathOL.RenderDeathOL ();
+        driftGauge.fillAmount = 0;
 
-		deathDisposable = Observable.Timer (TimeSpan.FromSeconds (deathDelay))
-			.Subscribe (_ => {
-				ship.gameObject.SetActive (true);
-				deathGroup.SetActive (false);
+        actualSpeed = 0;
+        actualRotStrength = 0;
 
-				driftTime = maxDriftTime;
+        death = true;
+        jump = false;
+        invulnerability = true;
+        shield = false;
 
-				transform.position = spawnPos;
-				transform.rotation = Quaternion.identity;
-
-				death = false;
-
-				invulDisposable = Observable.Timer(TimeSpan.FromSeconds(spawnTimeProtection))
-					.Subscribe(
-						__ => {if (jump) {invulnerability = false;}},
-						() => {
-							invulDisposable.Dispose();
-							invulDisposable = null;
-						})
-					.AddTo(this);
-				
-		},
-				() => {
-					deathDisposable.Dispose();
-					deathDisposable = null;
-				}).AddTo (this);
-	}*/
-
-	public void Death()
-	{
-		if (!shield)
-		{
-			if (cor != null)
-				StopCoroutine(cor);
-			cor = StartCoroutine(CoDeath());
-		}
-	}
-
-	public IEnumerator CoDeath()
-	{
-		string id = player.ToString();
-		id = id.Substring(id.Length - 1);
-		Vector3 deathPos = transform.position;
-
-		SlowMo.selfAnimator.Play("Anim_SlowMo", 0, 0);
-
-		ship.gameObject.SetActive(false);
-		ship.GetComponent<MeshRenderer> ().material = baseMat;
-		deathGroup.SetActive(true);
-
-		foreach (ParticleSystem ps in firePS)
-			ps.emissionRate = 0;
-		foreach (ParticleSystem ps in driftPS)
-			ps.emissionRate = 0;
-
-		selfRB.velocity = Vector3.zero;
-
-		driftGauge.fillAmount = 0;
-
-		actualSpeed = 0;
-		actualRotStrength = 0;
-
-		death = true;
-		jump = false;
-		invulnerability = true;
-		shield = false;
-
-		deathOL.RenderDeathOL();
+        deathOL.RenderDeathOL();
 
         // Only for lobby.
-        if (GameManager.Instance.IsInGame == false)
-        {
+        if (GameManager.Instance.IsInGame == false) {
             GameManager.Instance.Players[playerID].SetActive(false);
             invulnerability = false;
 
@@ -373,50 +301,46 @@ public class ShipBehaviour_V2 : MonoBehaviour
             yield break;
         }
 
-		MessagingCenter.Instance.FireMessage("PlayerDeath",
-			new object[] { int.Parse(id), deathPos });
+        MessagingCenter.Instance.FireMessage(
+            "PlayerDeath",
+            new object[] { int.Parse(id), deathPos });
 
-		yield return new WaitForSeconds(deathDelay);
+        yield return new WaitForSeconds(deathDelay);
 
-		ship.gameObject.SetActive(true);
-		deathGroup.SetActive(false);
+        ship.gameObject.SetActive(true);
+        deathGroup.SetActive(false);
 
-		driftTime = maxDriftTime;
+        driftTime = maxDriftTime;
 
-		transform.position = spawnPos;
-		transform.rotation = Quaternion.identity;
+        transform.position = spawnPos;
+        transform.rotation = Quaternion.identity;
 
-		death = false;
+        death = false;
 
         yield return new WaitForSeconds(spawnTimeProtection);
 
-		invulnerability = false;
+        invulnerability = false;
     }
 
-	public void Respawn()
-	{
+    public void Respawn() {
 
-		ship.gameObject.SetActive(true);
-		deathGroup.SetActive(false);
+        ship.gameObject.SetActive(true);
+        deathGroup.SetActive(false);
 
-		driftTime = maxDriftTime;
+        driftTime = maxDriftTime;
 
-		transform.position = spawnPos;
-		transform.rotation = Quaternion.identity;
+        transform.position = spawnPos;
+        transform.rotation = Quaternion.identity;
 
-		death = false;
+        death = false;
     }
 
-	public void Land()
-	{
+    public void Land() {
+        jump = false;
+    }
 
-		jump = false;
-	}
-
-	public void RemoveAirProtection()
-	{
-
-		airProtection = false;
-		invulnerability = false;
-	}
+    public void RemoveAirProtection() {
+        airProtection = false;
+        invulnerability = false;
+    }
 }
